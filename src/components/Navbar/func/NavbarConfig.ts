2db55ac6b8b779de9046b053a80a55c7c4d4bdd2
Renstrio24p <waren.gador15@gmail.com>
2024-07-25 17:33:41 +0800

    add hooks update

--- src/components/Navbar/func/NavbarConfig.ts
+++ src/components/Navbar/func/NavbarConfig.ts
@@ -1,4 +1,4 @@
 export const config = {
-  ALLOWED_ATTR: ["width", "height", "class", "id", "src"],
+  ALLOWED_ATTR: ["width", "height", "class", "id", "src", "alt"],
   ALLOW_UNSAFE_SCRIPT: false,
 };
